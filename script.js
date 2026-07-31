541a50e26f1717ebc3ac9befb81ec4cd45a5139b
miguelalvarado1512usa-blip <miguelalvarado1512usa@gmail.com>
2026-07-31 03:41:32 -0400

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function addTask() {
   
   // Texto de la tarea (hacer clic para marcar como hecha)
   const span = document.createElement('span');
-  `[se que la paja anotala pa por las mavinas] ${taskText}`;
+  span.textContent =`[se que la paja anotala pa por las mavinas] ${taskText}`;
   span.addEventListener('click', () => {
     li.classList.toggle('completed');
   });
