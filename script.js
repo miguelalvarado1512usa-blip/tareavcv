54aa54975df9125bee182d9627ecd86282a02c2f
miguelalvarado1512usa-blip <miguelalvarado1512usa@gmail.com>
2026-07-31 04:06:36 -0400

    Fix task input and update total count display

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 const taskInput = document.getElementById('taskInput');
 const addBtn = document.getElementById('addBtn');
 const taskList = document.getElementById('taskList');
+const totalCountElement = document.getElementById('totalCount');
 
+// Variable matemática para llevar la cuenta
 let totalCagadas = 0;
 
 addBtn.addEventListener('click', addTask);
@@ -10,27 +12,27 @@ function addTask() {
   const taskText = taskInput.value.trim();
   if (taskText === '') return;
 
+  // 1. Sumamos 1 al contador
   totalCagadas++;
-
+  // 2. Actualizamos el texto en pantalla
   totalCountElement.textContent = `Total acumulado: 💩 ${totalCagadas}`;
-  
+
   const li = document.createElement('li');
   
-  // Texto de la tarea (hacer clic para marcar como hecha)
   const span = document.createElement('span');
-  span.textContent =`mamaguevo${taskText}`;
+  span.textContent = `💩 [sapo] ${taskText}`;
   
   span.addEventListener('click', () => {
     li.classList.toggle('completed');
   });
 
-  // Botón para eliminar
   const deleteBtn = document.createElement('button');
   deleteBtn.textContent = 'X';
   deleteBtn.className = 'delete-btn';
+  
   deleteBtn.addEventListener('click', () => {
     li.remove();
-  // Restamos 1 si borras la tarea
+    // Restamos 1 si borras la tarea
     totalCagadas--;
     totalCountElement.textContent = `Total acumulado: 💩 ${totalCagadas}`;
   });
